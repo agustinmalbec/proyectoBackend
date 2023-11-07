7d631c9e7edb2eb38b5237cbc86229d882acd492
agustinmalbec <agustinmalbec123@gmail.com>
2023-11-07 12:50:02 -0300

    Segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,61 +1,121 @@
+import { log } from 'console';
+import fs from 'fs';
+
 class ProductManager {
 
-    constructor() {
-        this.products = [];
+    constructor(path) {
+        this.path = path;
+        if (fs.existsSync(path)) {
+            try {
+                let products = fs.readFileSync(path, 'utf-8');
+                this.products = JSON.parse(products);
+            } catch (error) {
+                this.products = [];
+            }
+        } else {
+            this.products = [];
+        }
+
+    }
+
+    async saveFile(data) {
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify(data, null, '\t')
+            );
+            return true;
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
+            return false;
+        }
     }
 
     getProducts() {
-        return this.products;
+        try {
+            return this.products;
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
+        }
     }
 
     getProductById(productId) {
-        const product = this.products.find((finded) => finded.id === productId);
-        if (product) {
-            return product;
-        } else {
-            return console.log(`Not found ID ${productId}`);
+        try {
+            const product = this.products.find((finded) => finded.id === productId);
+            if (product) {
+                return product;
+            } else {
+                return console.log(`Not found ID ${productId}`);
+            }
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
         }
     }
 
-    addProduct(product) {
-        if (!product.title) {
-            return console.log("Falta el campo TITLE");
-        }
-        if (!product.description) {
-            return console.log("Falta el campo DESCRIPTION");
-        }
-        if (!product.price) {
-            return console.log("Falta el campo PRICE");
-        }
-        if (!product.thumbnail) {
-            return console.log("Falta el campo THUMBNAIL");
-        }
-        if (!product.code) {
-            return console.log("Falta el campo CODE");
-        }
-        if (!product.stock) {
-            return console.log("Falta el campo STOCK");
-        }
-
-        const find = this.products.find((finded) => finded.code === product.code);
-        if (find === undefined) {
-            if (this.products.length === 0) {
-                product.id = 1;
-            } else {
-                product.id = this.products[this.products.length - 1].id + 1;
+    async addProduct(product) {
+        try {
+            const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+            for (let field of requiredFields) {
+                if (!product[field]) {
+                    return console.log(`Falta el campo ${field}`);
+                }
             }
-            return this.products.push(product);
-        } else {
-            return console.log("El campo CODE se encuentra repetido");
+
+            const find = this.products.find((finded) => finded.code === product.code);
+            if (find !== undefined) {
+                return console.log("El campo CODE se encuentra repetido");
+            }
+
+            product.id = this.products.length > 0 ? this.products[this.products.length - 1].id + 1 : 1;
+            this.products.push(product);
+
+            const response = await this.saveFile(this.products);
+            if (response) {
+                console.log("Usuario agregado correctamente");
+            } else {
+                console.log("Hubo un error al crear un usuario");
+            }
+
+            return product;
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
+        }
+    }
+
+    async updateProduct(productId, update) {
+        try {
+            const product = this.products.find((prod) => prod.id === productId);
+            const index = this.products.findIndex(prod => prod.id === productId);
+            if (index === -1) {
+                return console.log('No se encontro el producto');
+            }
+            update.id = product.id;
+            this.products[index] = update;
+            await this.saveFile(this.products);
+            return console.log("Actualizado correctamente");
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
+        }
+    }
+
+    async deleteProduct(productId) {
+        try {
+            const index = this.products.findIndex(prod => prod.id === productId);
+            if (index === -1) {
+                return console.log('No se encontro el producto');
+            }
+            this.products.splice(index, 1);
+            await this.saveFile(this.products);
+            return console.log("Eliminado correctamente");
+        } catch (error) {
+            console.log(`Ha ocurrido un error: ${error}`);
         }
     }
 }
 
 const producto1 = { title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail: 'Sin imagen', code: 'abc123', stock: 25 };
 
-const manager = new ProductManager();
-console.log(manager.getProducts());
-manager.addProduct(producto1);
+const manager = new ProductManager('./products.json');
+/* console.log(manager.getProducts());
 manager.addProduct(producto1);
 manager.getProductById(2);
-console.log(manager.getProducts());
+console.log(manager.getProducts()); */
+manager.deleteProduct(1);
